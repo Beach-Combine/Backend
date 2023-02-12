07468e104b92beba2213f2ddefb9295ea79330b3
Choo <choo000407@naver.com>
2023-02-12 17:27:44 +0900

    Feat: 구글 로그인 시 정보 없으면 새 member 생성

--- src/main/java/beachcombine/backend/controller/JwtController.java
+++ src/main/java/beachcombine/backend/controller/JwtController.java
@@ -1,5 +1,8 @@
 package beachcombine.backend.controller;
 
+import beachcombine.backend.common.oauth.provider.GoogleUser;
+import beachcombine.backend.common.oauth.provider.OAuthUserInfo;
+import beachcombine.backend.domain.Member;
 import beachcombine.backend.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,6 +27,23 @@ public class JwtController {
     public String jwtCreate(@RequestBody Map<String, Object> data) {
         System.out.println("jwtCreate 실행됨");
         System.out.println(data.get("profileObj"));
+
+        OAuthUserInfo googleUser = new GoogleUser((Map<String, Object>)data.get("profileObj"));
+
+        Member memberEntity = memberRepository.findByLoginId(googleUser.getProvider()+"_"+googleUser.getProviderId());
+
+        if(memberEntity == null) {
+            Member memberRequest = Member.builder()
+                    .loginId(googleUser.getProvider()+"_"+googleUser.getProviderId())
+                    .password(bCryptPasswordEncoder.encode("password"))
+                    .email(googleUser.getEmail())
+                    .provider(googleUser.getProvider())
+                    .providerId(googleUser.getProviderId())
+                    .role("ROLE_USER")
+                    .build();
+
+            memberEntity = memberRepository.save(memberRequest);
+        }
 
         // 구현 예정
         return "";
