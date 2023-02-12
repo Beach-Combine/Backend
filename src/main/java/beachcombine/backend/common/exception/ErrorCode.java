26c57ba8c4a46e2a400142cb9a0d471dc0271cd6
Choo <choo000407@naver.com>
2023-02-12 22:13:33 +0900

    Feat: 토큰, 경로 예외 ErrorCode 추가

--- src/main/java/beachcombine/backend/common/exception/ErrorCode.java
+++ src/main/java/beachcombine/backend/common/exception/ErrorCode.java
@@ -16,7 +16,14 @@ public enum ErrorCode {
     UNAUTHORIZED_ID(HttpStatus.UNAUTHORIZED, "아이디가 틀립니다."),
     UNAUTHORIZED_PASSWORD(HttpStatus.UNAUTHORIZED, "패스워드가 틀립니다."),
     NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "해당 유저를 찾을 수 없습니다."),
-    EXIST_USER_NICKNAME(HttpStatus.CONFLICT, "이미 존재하는 닉네임입니다.");
+    EXIST_USER_NICKNAME(HttpStatus.CONFLICT, "이미 존재하는 닉네임입니다."),
+
+    //토큰 예외
+    TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다."),
+    TOKEN_INVALID(HttpStatus.UNAUTHORIZED, "토큰이 유효하지 않습니다."),
+
+    //경로 예외
+    NOT_VALID_URI(HttpStatus.BAD_REQUEST, "유효한 경로로 요청해주세요.");
 
     private final HttpStatus httpStatus;
     private final String detail;
