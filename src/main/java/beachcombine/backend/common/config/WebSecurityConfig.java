7d69cf0469efccf20198896660019b2c89d269e9
Choo <choo000407@naver.com>
2023-02-12 18:19:11 +0900

    Feat: security에 jwt 필터 설정

--- src/main/java/beachcombine/backend/common/config/WebSecurityConfig.java
+++ src/main/java/beachcombine/backend/common/config/WebSecurityConfig.java
@@ -1,5 +1,8 @@
 package beachcombine.backend.common.config;
 
+import beachcombine.backend.common.jwt.JwtAuthenticationFilter;
+import beachcombine.backend.common.jwt.JwtAuthorizationFilter;
+import beachcombine.backend.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +18,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CorsConfig corsConfig;
+    private final MemberRepository memberRepository;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -30,6 +34,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .addFilter(corsConfig.corsFilter()) // @CrossOrigin(인증X), 시큐리티 필터에 등록(인증O)
                 .formLogin().disable() // jwt 서버라서 아이디,비밀번호를 formLogin으로 안함
                 .httpBasic().disable() // 매 요청마다 id, pwd 보내는 방식으로 인증하는 httpBasic 사용X
+                .addFilter(new JwtAuthenticationFilter((authenticationManager())))
+                .addFilter(new JwtAuthorizationFilter(authenticationManager(), memberRepository))
                 .authorizeRequests() // 요청에 대한 사용권한 체크
                 .antMatchers("/oauth2/*").permitAll()
                 .anyRequest().permitAll();
