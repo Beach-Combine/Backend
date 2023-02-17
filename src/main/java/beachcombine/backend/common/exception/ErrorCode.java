0b456f966ac1f815da31c4e2623b5b17b6aa31c8
BK <kbomin328@gmail.com>
2023-02-17 22:52:52 +0900

    Feat: 해변 뱃지 조회 API 구현

--- src/main/java/beachcombine/backend/common/exception/ErrorCode.java
+++ src/main/java/beachcombine/backend/common/exception/ErrorCode.java
@@ -19,6 +19,9 @@ public enum ErrorCode {
     EXIST_USER_NICKNAME(HttpStatus.CONFLICT, "이미 존재하는 닉네임입니다."),
     BAD_REQUEST_OPTION_VALUE(HttpStatus.BAD_REQUEST, "option 값이 잘못된 요청입니다."),
 
+    // Beach 예외
+    NOT_FOUND_BEACH(HttpStatus.NOT_FOUND, "해당 해변을 찾을 수 없습니다."),
+
     //토큰 예외
     TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다."),
     TOKEN_INVALID(HttpStatus.UNAUTHORIZED, "토큰이 유효하지 않습니다."),
