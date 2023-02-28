8748bd23213e21d5c84ed8439aba54b2c0bd15d3
Choo <choo000407@naver.com>
2023-02-28 21:50:17 +0900

    Chore: QueryDsl 관련 설정

---
+++ src/main/java/beachcombine/backend/common/config/DBConfig.java
@@ -0,0 +1,17 @@
+package beachcombine.backend.common.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+
+@Configuration
+public class DBConfig {
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+
+        return new JPAQueryFactory(entityManager);
+    }
+}
