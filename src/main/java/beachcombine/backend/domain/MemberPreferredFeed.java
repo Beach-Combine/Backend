654b6615025db8af6639c5d18386e552859b7033
Choo <choo000407@naver.com>
2023-03-06 22:25:32 +0900

    Add: MemberPreferredFeed 도메인 추가

---
+++ src/main/java/beachcombine/backend/domain/MemberPreferredFeed.java
@@ -0,0 +1,34 @@
+package beachcombine.backend.domain;
+
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name="MEMBER_PREFERRED_ARTIST", uniqueConstraints = {@UniqueConstraint(
+        name="MEMBER_FEED_UNIQUE",
+        columnNames = {"MEMBER_ID", "FEED_ID"}
+)})
+public class MemberPreferredFeed {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_preferred_feed_id")
+    private Long id;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Member member;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name = "feed_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Feed feed;
+}
