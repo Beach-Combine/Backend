6c8aeedd321cc9857dcf8a504456777245b9864b
Choo <choo000407@naver.com>
2023-03-12 01:36:01 +0900

    Add: Giftcard, Member, Store entity 추가, 연관관계 설정
    Giftcard별로 구매 기록 볼 필요 X -> 단방향
    Member별로 구매 기록 볼 필요 O -> 양방향
    Store별로 기프트 카드 목록 볼 필요 X -> 단방향

---
+++ src/main/java/beachcombine/backend/domain/Store.java
@@ -0,0 +1,24 @@
+package beachcombine.backend.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Store {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "store_id")
+    private Long id;
+
+    private String name;
+    private String location;
+}
